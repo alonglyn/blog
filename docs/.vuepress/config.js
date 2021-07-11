8611a9ba364eb0fdf11300a64651c72b81a2e053
alonglyn <alonglyn.0@gmail.com>
2021-07-12 02:44:56 +0800

    vuepress-theme-hope.github.io/md-enhance/zh plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,18 +2,25 @@ module.exports = {
     title: 'Along\'s programming blog',
     description: 'programming, learning or working',
     // base:'https://github.com/alonglyn/blog'
-
+    //markdown
     //theme
     themeConfig: {
         nav: [
             {
-                text: '网站搭建', 
+                text: '网站搭建',
                 items: [
-                    {text: '搭建教程参考', link:'https://coder.itclan.cn/fontend/tools/vuepress-build-blog/#%E5%9F%BA%E6%9C%AC%E9%85%8D%E7%BD%AE'},
-                    {text: '博客仓库地址', link:'https://github.com/alonglyn/blog'},
+                    { text: '搭建教程参考', link: 'https://coder.itclan.cn/fontend/tools/vuepress-build-blog/#%E5%9F%BA%E6%9C%AC%E9%85%8D%E7%BD%AE' },
+                    { text: '博客仓库地址', link: 'https://github.com/alonglyn/blog' },
                 ]
             }
         ],
-        sidebar:'auto',
-    }
+        sidebar: 'auto',
+    }, plugins: [
+        [
+            "md-enhance",
+            {
+                enableAll:true,
+            },
+        ],
+    ],
 }

0c62e32b7dba17bda35f73b222f54aa4aaa92eb2
linzhonglong <linzhonglong@bytedance.com>
2021-07-12 14:04:32 +0800

    完成第一章笔记

--- docs/blog/tech/test.cpp
+++ docs/blog/tech/test.cpp
@@ -6,6 +6,12 @@ int main() {
     short d = (short)c;
     int e = 0xffff7fff;
     short f = (short)e;
-    printf("%d->%d, %d->%d, %d->%d", a,b,c,d,e,f);
+    printf("%d->%d, %d->%d, %d->%d\n", a,b,c,d,e,f);
+    int x = 0x00007fab;
+    unsigned char *point = (unsigned char *)&x;
+    size_t len = sizeof(int)/sizeof(char);
+    for (size_t i=0;i<len;i++) {
+        printf("%p\t0x%.2x\n", point+i, *(point+i));
+    }
     return 0;
 }
